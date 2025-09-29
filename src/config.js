395b296b61763ca4155b56f7c3f16bd86875267f
scientist-Momi <scientistmomi22@gmail.com>
2025-09-29 14:39:25 +0100

    updating browser optimisation

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL_LOCAL = 'http://localhost:8080/api/v1'
-export const API_BASE_URL = 'https://koob-backend-app-a443b385149d.herokuapp.com/api/v1'
-export const API_BASE = 'https://koob-backend-app-a443b385149d.herokuapp.com'
+export const API_BASE_URL = 'https://api.koob.oolumomi.dev/api/v1'
+export const API_BASE = 'https://api.koob.oolumomi.dev'
 
 // http://localhost:8080/login/oauth2/code/google
